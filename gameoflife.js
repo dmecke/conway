5029a429a4c5de0b7841816808e8b09f6b801dca
Daniel Mecke <daniel.mecke@cunningsoft.de>
2014-06-07 21:38:52 +0200

    finalized basic game

--- gameoflife.js
+++ gameoflife.js
@@ -1,7 +1,10 @@
-var speed = [500,1000,2000,100];
 var presets = [[666,704,706,734,735,742,743,773,777,782,783,796,797,802,803,812,818,822,823,836,837,842,843,852,856,858,859,864,866,892,898,906,933,937,974,975],[780,820,860],[780,818,819,860,861,899],[777,782,815,816,818,819,820,821,823,824,857,862],[780,818,820,859,860],[779,782,818,858,862,898,899,900,901],[699,700,701,739,741,779,781,859,861,899,901,939,940,941],[780,781,819,820,860],[575,576,582,583,616,617,621,622,653,656,658,660,662,665,693,694,695,697,698,700,701,703,704,705,734,736,738,740,742,744,775,776,777,781,782,783,855,856,857,861,862,863,894,896,898,900,902,904,933,934,935,937,938,940,941,943,944,945,973,976,978,980,982,985,1016,1017,1021,1022,1055,1056,1062,1063]];
 var presetnames = [["Glider gun."],["Blinker."],["Clock."],["Pulsator."],["Glider."],["Spaceship."],["Bracket."],["F-pentomino."],["Pulsar."]];
-var generation, cell, interval;
+var generation, cell, cellLastGeneration, cellLastLastGeneration, interval;
+
+Array.prototype.clone = function() {
+    return this.slice(0);
+};
 
 function setup()
 {
@@ -10,6 +13,8 @@ function setup()
     for (var i = 0; i < 1600; i++) {
         setCellDead(i);
     }
+    cellLastGeneration = cell.clone();
+    cellLastLastGeneration = cell.clone();
     showMessage('Click to place living cells.');
     initializeScreen();
 }
@@ -49,27 +54,44 @@ function draw(i)
     show();
 }
 
+function startstop()
+{
+    if (interval) {
+        stop();
+    } else {
+        start();
+    }
+}
+
 function start()
 {
-    $('.menu_active').show();
-    $('.menu_paused').hide();
-    interval = window.setInterval(step, speed[0]);
+    interval = window.setInterval(step, 100);
+    $('#startstop').html('Stop');
 }
 
 function stop()
 {
     window.clearInterval(interval);
-    $('.menu_active').hide();
-    $('.menu_paused').show();
+    interval = null;
     show();
+    $('#startstop').html('Start');
 }
 
 function step()
 {
+    cellLastLastGeneration = cellLastGeneration.clone();
+    cellLastGeneration = cell.clone();
     generation++;
-    showMessage('Generations: ' + generation);
     updateCellStatus();
+    var isStableMessage = '';
+    if (isStable()) {
+        isStableMessage += ' STABLE!';
+    }
+    showMessage('Generations: ' + generation + isStableMessage);
     show();
+    if (isStable()) {
+        stop();
+    }
 }
 
 function updateCellStatus()
@@ -80,7 +102,7 @@ function updateCellStatus()
             setCellAlive(i);
         }
         if (cell[i].alive && (numb[i] < 2 || numb[i] > 3)) {
-            setCellWasAlive(i);
+            setCellDead(i);
         }
     }
 }
@@ -103,26 +125,6 @@ function findNeighboursAlive()
     }
 
     return numb;
-}
-
-function faster()
-{
-    if (speed[0] != 100) {
-        speed.unshift(speed[speed.length - 1]);
-        speed.length--;
-        window.clearInterval(interval);
-        interval = window.setInterval(step, speed[0]);
-    }
-}
-
-function slower()
-{
-    if (speed[0] != 2000) {
-        speed.push(speed[0]);
-        speed.shift();
-        window.clearInterval(interval);
-        interval = window.setInterval(step, speed[0]);
-    }
 }
 
 function preset()
@@ -177,10 +179,13 @@ function setCellDead(index)
     }
 }
 
-function setCellWasAlive(index)
+function isStable()
 {
-    cell[index] = {
-        alive: false,
-        status: 'wasAlive'
+    for (var i = 0; i < 1600; i++) {
+        if (cell[i].alive != cellLastGeneration[i].alive && cell[i].alive != cellLastLastGeneration[i].alive) {
+            return false;
+        }
     }
+
+    return true;
 }
